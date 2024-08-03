8c18a7aeea916b74da39447c1368b7ed0e4e4753
jonas <jonas.peixoto.087@gmail.com>
2024-08-03 18:22:28 -0300

    #Feat: Adicionando metodo get

--- extracao_api_twitter.py
+++ extracao_api_twitter.py
@@ -1,5 +1,19 @@
 from datetime import datetime, timedelta
+import os 
+import requests
 
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H; %M: %S.00Z"
 ent_time = datetime.now().strftime(TIMESTAMP_FORMAT)
 start_time = (datetime.now() + timedelta(-1)).date()
+
+query = "data science"
+
+url_raw = f'https://api.twitter.com/2/tweets/search/recent?query={query}'
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+headers = {"Authorization" : "Bearer {}".format(bearer_token)}
+reponse = requests.request("GET", url_raw, headers=headers)
+
+
+
+
